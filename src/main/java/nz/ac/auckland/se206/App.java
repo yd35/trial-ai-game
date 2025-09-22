e712973c619b0bcbf1731236c18a4626724cd2ef
miketomotimo <mset621@aucklanduni.ac.nz>
2025-09-22 12:41:02 +1200

    Make stage unresizeable.

--- src/main/java/nz/ac/auckland/se206/App.java
+++ src/main/java/nz/ac/auckland/se206/App.java
@@ -21,6 +21,7 @@ public class App extends Application {
     Parent root = FXMLLoader.load(getClass().getResource("/fxml/menu.fxml"));
   
     Scene scene = new Scene(root);
+    stage.setResizable(false);
 
     stage.setScene(scene);
     stage.show();
