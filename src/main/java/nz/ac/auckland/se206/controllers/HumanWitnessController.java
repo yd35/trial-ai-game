0fe2b7d24fff299d423d6cb25ce3bdf855730a54
LouisCao1029 <lcao350@aucklanduni.ac.nz>
2025-09-23 02:14:20 +1200

    added the controllers for all 3 chats and the opening scene, (flashback not done yet so we cant see the chats since mainmenu goes into flashbacks before the chats). implemented goBackButton. sendMessage not implemented

---
+++ src/main/java/nz/ac/auckland/se206/controllers/HumanWitnessController.java
@@ -0,0 +1,37 @@
+package nz.ac.auckland.se206.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
+import nz.ac.auckland.se206.SceneManager;
+import nz.ac.auckland.se206.SceneManager.AppUi;
+
+public class HumanWitnessController {
+  @FXML private Text timerText;
+  @FXML private Button goBackButton;
+  @FXML private TextArea chatTextArea;
+  @FXML private Button sendButton;
+  @FXML private ImageView image;
+  @FXML private TextField textField;
+  @FXML private ImageView memoryscape;
+
+  @FXML
+  private void onGoBack(ActionEvent event) {
+    Button button = (Button) event.getSource();
+    Scene sceneButtonIsIn = button.getScene();
+    sceneButtonIsIn.setRoot(SceneManager.getUiRoot(AppUi.MAINMENU));
+  }
+  
+  @FXML
+  private void sendMessage(ActionEvent event) {
+  }
+
+  @FXML
+  private void initialize() {
+  }
+}
