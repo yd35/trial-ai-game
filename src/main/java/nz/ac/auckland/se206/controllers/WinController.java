826fafe2d29f686049acfd50f242e313f251607b
Aditeya Anand <aana050@aucklanduni.ac.nz>
2025-09-24 00:54:54 +1200

    Game now resets properly after decision has been made

--- src/main/java/nz/ac/auckland/se206/controllers/WinController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/WinController.java
@@ -1,21 +1,11 @@
 package nz.ac.auckland.se206.controllers;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.text.Text;
-import nz.ac.auckland.se206.SceneManager;
-import nz.ac.auckland.se206.SceneManager.AppUi;
+import nz.ac.auckland.se206.App;
 
 public class WinController {
-  @FXML private Text titleText;
-  @FXML private Button playAgainButton;
-
   @FXML
-  private void onPlayAgain(ActionEvent event) {
-    Button button = (Button) event.getSource();
-    Scene sceneButtonIsIn = button.getScene();
-    sceneButtonIsIn.setRoot(SceneManager.getUiRoot(AppUi.OPENING));
+  private void onPlayAgain() {
+    App.resetAndGoToMenu();
   }
 }
