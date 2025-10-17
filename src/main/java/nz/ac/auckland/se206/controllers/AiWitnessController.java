ec5775d67d2645cf3721b0f83e36b9f7c14465a6
miketomotimo <mset621@aucklanduni.ac.nz>
2025-10-18 12:24:13 +1300

    Added initial message to AI witness. Explains the puzzle as well.

--- src/main/java/nz/ac/auckland/se206/controllers/AiWitnessController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/AiWitnessController.java
@@ -64,6 +64,13 @@ public class AiWitnessController {
 
   private GptClient client;
   private ChatMessage systemPrompt;
+  private static final String startingText =
+      "ORACLE: My analysis requires displaying the retrieved drone data. Due to chemical damage,"
+          + " the control interface is limited. You must manipulate the data using the operational"
+          + " input buttons: [-1] and [3]. The system integrity will tolerate a maximum of four"
+          + " button presses to achieve the required sum of 4. Should an error occur, input the [R]"
+          + " command next to the [3] button to reset the sequence. Select your sequence of four"
+          + " inputs with maximum efficiency.";
 
   /* AI witness memory puzzle
     --------------
@@ -247,7 +254,6 @@ public class AiWitnessController {
 
     systemPrompt = new ChatMessage("system", PromptEngineering.getPrompt("aiWitness"));
 
-    String startingText = "ORACLE: initial message";
     chatTextArea.appendText(startingText + "\n\n");
 
     SharedTimer timer = SharedTimer.getInstance();
